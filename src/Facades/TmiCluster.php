fb540a4010dc4ee4e744646d94344e4a253e23e8
René Preuß <rene@preuss.io>
2022-04-26 11:22:08 +0200

    Add facade documentation

--- src/Facades/TmiCluster.php
+++ src/Facades/TmiCluster.php
@@ -5,6 +5,22 @@ namespace GhostZero\TmiCluster\Facades;
 use GhostZero\TmiCluster\TmiCluster as TmiClusterService;
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @see \GhostZero\TmiCluster\TmiCluster::routes
+ * @method static void routes($callback = null, array $options = [])
+ * @see \GhostZero\TmiCluster\Traits\TmiClusterHelpers::sendMessage
+ * @method static void sendMessage(string $channel, $message)
+ * @see \GhostZero\TmiCluster\TmiCluster::use
+ * @method static void use(string $connection)
+ * @see \GhostZero\TmiCluster\Traits\TmiClusterHelpers::joinNextServer
+ * @method static void joinNextServer(array $channels, array $staleIds = [])
+ * @see \GhostZero\TmiCluster\TmiCluster::routeSlackNotificationsTo
+ * @method static void routeSlackNotificationsTo(string $url, string $channel = null)
+ * @see \GhostZero\TmiCluster\TmiCluster::routeSmsNotificationsTo
+ * @method static void routeSmsNotificationsTo(string $number)
+ * @see \GhostZero\TmiCluster\TmiCluster::routeMailNotificationsTo
+ * @method static void routeMailNotificationsTo(string $email)
+ */
 class TmiCluster extends Facade
 {
     protected static function getFacadeAccessor()
