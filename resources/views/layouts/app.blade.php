bb150ab3f4589940ef579a10b100358026d6eac0
René Preuß <rene@preuss.io>
2022-10-16 20:48:00 +0200

    Change to fully GDPR-compliant CDN urls

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,18 +9,23 @@
     @stack('tmi-cluster::meta')
 
     <link rel="canonical" href="{{ request()->url() }}">
-    <link rel="icon" type="image/x-icon" href="{{ GhostZero\TmiCluster\Support\Url::asset('vendor/tmi-cluster/favicon.ico') }}">
+    <link rel="icon" type="image/x-icon"
+          href="{{ GhostZero\TmiCluster\Support\Url::asset('vendor/tmi-cluster/favicon.ico') }}">
 
     <!-- Fonts & Core CSS -->
-    <link href="https://fa-cdn.bitinflow.com/releases/v5.13.0/css/all.min.css" rel="stylesheet"
-          crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;700;900&display=swap"
+    <link rel="stylesheet" href="https://cdn.bitinflow.com/fontawesome/6.2.0/css/all.min.css"
+          integrity="sha384-a/J8ePf6YlZ5aBQ6xmsPiCGV52z4VJMixEqX+4Mvoxi8flAWQA00ZFtk5qO5vyrP" crossorigin="anonymous">
+    <link href="https://fonts.bunny.net/css2?family=Inter:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet" crossorigin="anonymous">
-    <link rel="preload" as="font" href="https://fa-cdn.bitinflow.com/fonts/JetBrainsMono-ExtraBold.woff2" type="font/woff2"
-          crossorigin="anonymous">
-    <link rel="preload" as="font" href="https://fa-cdn.bitinflow.com/fonts/JetBrainsMono-Regular.woff2" type="font/woff2"
-          crossorigin="anonymous">
-    <link href="{{ GhostZero\TmiCluster\Support\Url::asset(mix('tmi-cluster.css', 'vendor/tmi-cluster')) }}" rel="stylesheet">
+    <link rel="preload" as="font" href="https://cdn.bitinflow.com/fonts/JetBrainsMono-ExtraBold.woff2"
+          integrity="sha384-O90Ey4hwu+amqcdiaazAUkVFmW9Rtvj7Muru0dYjEUGdiRYIRztSyIOwV5OmOfY9"
+          type="font/woff2" crossorigin="anonymous">
+    <link rel="preload" as="font" href="https://cdn.bitinflow.com/fonts/JetBrainsMono-Regular.woff2"
+          integrity="sha384-QMyniVbTk4oi/X4p4HSbfku1TOppZPLcfrcflL8DwBuVOdMbBpodAfBr5Wa4AOMK"
+          type="font/woff2" crossorigin="anonymous">
+    <link href="{{ asset(mix('tmi-cluster.css', 'vendor/tmi-cluster')) }}" rel="stylesheet">
+    <link href="{{ GhostZero\TmiCluster\Support\Url::asset(mix('tmi-cluster.css', 'vendor/tmi-cluster')) }}"
+          rel="stylesheet">
 
     <!-- Theme -->
     <meta name="theme-color" content="#6d00ff">
@@ -36,7 +41,9 @@
     </footer>
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"></script>
+<script src="https://cdn.bitinflow.com/chart.js/2.9.3/Chart.min.js"
+        integrity="sha384-i+dHPTzZw7YVZOx9lbH5l6lP74sLRtMtwN2XjVqjf3uAGAREAF4LMIUDTWEVs4LI"
+        crossorigin="anonymous"></script>
 <script src="{{ GhostZero\TmiCluster\Support\Url::asset(mix('tmi-cluster.js', 'vendor/tmi-cluster')) }}"></script>
 </body>
 </html>
--- resources/views/layouts/astro.blade.php
+++ resources/views/layouts/astro.blade.php
@@ -12,14 +12,16 @@
     <link rel="icon" type="image/x-icon" href="{{ asset('vendor/tmi-cluster/favicon.ico') }}">
 
     <!-- Fonts & Core CSS -->
-    <link href="https://fa-cdn.bitinflow.com/releases/v5.13.0/css/all.min.css" rel="stylesheet"
-          crossorigin="anonymous">
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;300;400;500;700;900&display=swap"
+    <link rel="stylesheet" href="https://cdn.bitinflow.com/fontawesome/6.2.0/css/all.min.css"
+          integrity="sha384-a/J8ePf6YlZ5aBQ6xmsPiCGV52z4VJMixEqX+4Mvoxi8flAWQA00ZFtk5qO5vyrP" crossorigin="anonymous">
+    <link href="https://fonts.bunny.net/css2?family=Inter:wght@100;300;400;500;700;900&display=swap"
           rel="stylesheet" crossorigin="anonymous">
-    <link rel="preload" as="font" href="https://fa-cdn.bitinflow.com/fonts/JetBrainsMono-ExtraBold.woff2" type="font/woff2"
-          crossorigin="anonymous">
-    <link rel="preload" as="font" href="https://fa-cdn.bitinflow.com/fonts/JetBrainsMono-Regular.woff2" type="font/woff2"
-          crossorigin="anonymous">
+    <link rel="preload" as="font" href="https://cdn.bitinflow.com/fonts/JetBrainsMono-ExtraBold.woff2"
+          integrity="sha384-O90Ey4hwu+amqcdiaazAUkVFmW9Rtvj7Muru0dYjEUGdiRYIRztSyIOwV5OmOfY9"
+          type="font/woff2" crossorigin="anonymous">
+    <link rel="preload" as="font" href="https://cdn.bitinflow.com/fonts/JetBrainsMono-Regular.woff2"
+          integrity="sha384-QMyniVbTk4oi/X4p4HSbfku1TOppZPLcfrcflL8DwBuVOdMbBpodAfBr5Wa4AOMK"
+          type="font/woff2" crossorigin="anonymous">
     <link href="{{ asset(mix('tmi-cluster.css', 'vendor/tmi-cluster')) }}" rel="stylesheet">
 
     <!-- Theme -->
@@ -33,7 +35,9 @@
     @yield('content')
 </div>
 
-<script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js"></script>
+<script src="https://cdn.bitinflow.com/chart.js/2.9.3/Chart.min.js"
+        integrity="sha384-i+dHPTzZw7YVZOx9lbH5l6lP74sLRtMtwN2XjVqjf3uAGAREAF4LMIUDTWEVs4LI"
+        crossorigin="anonymous"></script>
 <script src="{{ asset(mix('tmi-cluster.js', 'vendor/tmi-cluster')) }}"></script>
 </body>
 </html>
