ad5e67bfc57bc96d9237243b3e8a53642aab2c4a
René Preuß <rene@preuss.io>
2023-01-25 11:39:07 +0100

    Improve health command

--- src/Support/Health.php
+++ src/Support/Health.php
@@ -8,19 +8,43 @@ use Illuminate\Support\Collection;
 
 class Health
 {
+    /**
+     * Time in seconds after which a supervisor is considered unhealthy.
+     */
+    private const INRESPONSIVE_THRESHOLD = 60;
+
     public static function isOperational(Collection $supervisors): bool
     {
         $operational = $supervisors
             ->sum(function (Supervisor $supervisor) {
                 return $supervisor->processes->filter(function (SupervisorProcess $process) {
-                    return $process->state === SupervisorProcess::STATE_CONNECTED;
+                    return $this->isInConnectedState($process) || $this->isInInitialState($process);
                 })->count();
             });
+
         $count = $supervisors
             ->sum(function (Supervisor $supervisor) {
                 return $supervisor->processes->count();
             });
 
         return $count > 0 && $operational === $count;
+    }
+
+    /**
+     * Consider a process in initial state if state is in initialize and ping is within the threshold.
+     */
+    private function isInInitialState(SupervisorProcess $process): bool
+    {
+        return $process->state === SupervisorProcess::STATE_INITIALIZE
+            && $process->last_ping_at->diffInSeconds() < self::INRESPONSIVE_THRESHOLD;
+    }
+
+    /**
+     * Consider a process in connected state if state is in connected and ping is within the threshold.
+     */
+    private function isInConnectedState(SupervisorProcess $process): bool
+    {
+        return $process->state === SupervisorProcess::STATE_CONNECTED
+            && $process->last_ping_at->diffInSeconds() < self::INRESPONSIVE_THRESHOLD;
     }
 }
